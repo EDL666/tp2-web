acd4cd9ac35a224d9f93e1d8fc0c5657a690470b
EDL666 <samuellachance@hotmail.com>
2016-03-12 15:26:03 -0500

    Ajout des fonctions au main

--- projet/client/app/main/main.controller.js
+++ projet/client/app/main/main.controller.js
@@ -1,20 +1,22 @@
 'use strict';
 
-(function() {
-
-class MainController {
-
-  constructor($http) {
-    this.$http = $http;
-    this.awesomeThings = [];
-
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
-  }
-}
-
 angular.module('fs3App')
-  .controller('MainController', MainController);
+.controller('MainController', ['$scope','$http', function($scope,$http){
+  $scope.movies = [];
+  $scope.caca = "allo";
+  $http.get('https://www.omdbapi.com/?y=2016&s=batman&type=movie').then(
+    function successCallback(response) {
+      // Ce bloc sera exécuté lorsque la réponse sera reçue.
+      // Asynchrone
+      console.log(response);
+      console.log(response.data);
+      $scope.movies = response.data.Search;
+    },
+    function errorCallback(response) {
+      // Ce bloc sera exécuté lorsqu'une erreur est détectée.
+      // Asynchrone
+      console.log(response);
+    }
+  );
 
-})();
+}]);
