ffaf10b42b12a7643ac40b3a5845d98f6ee6040e
Eric <irakotoseheno@gmail.com>
2016-04-08 16:23:50 -0400

    LAAA LESS COOOMENNTS MAAARCHE SES PU PETER

--- projet/client/components/appxComment/appxComment.controller.js
+++ projet/client/components/appxComment/appxComment.controller.js
@@ -4,11 +4,11 @@ var app = angular.module('fs3App');
 app.controller('AppxCommentCtrl', ['$scope', 'postComment', function($scope,postComment) {
 	$scope.connexion = function(user){
 		var apiUrl = 'https://crispesh.herokuapp.com/api';
-    
+
 	};
-	
-	
-	
+
+
+
 	$scope.createComment = function(omdbid) {
 		var aPost = {body: $scope.commentText, movie_id: omdbid, status: 0};
 		postComment.save(aPost);
