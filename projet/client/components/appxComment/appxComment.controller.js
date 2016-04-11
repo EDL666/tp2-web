d308abb033436df8523524583d8d0016e2ca474f
EDL666 <samuellachance@hotmail.com>
2016-04-11 19:44:17 -0400

    delete fix

--- projet/client/components/appxComment/appxComment.controller.js
+++ projet/client/components/appxComment/appxComment.controller.js
@@ -12,16 +12,22 @@ app.controller('AppxCommentCtrl', ['$scope', 'postComment', 'commentServices', f
 		var aPost = {body: $scope.commentText, movie_id: omdbid, status: 0};
 		postComment.save(aPost);
 		$scope.refresh();
-	}
+	};
 	
 	$scope.deleteComment = function(commentID)
 	{
 		commentServices.delete({id:commentID});
-		$scope.refresh();
-	}
+		for (var i = 0; i < $scope.comments.length; i++)
+		{
+			if ($scope.comments[i].id == commentID)
+			{
+				delete $scope.comments[i];
+			}
+		}
+	};
 	
 	$scope.refresh = function()
 	{
 		window.location.reload();
-	}
+	};
 }]);
